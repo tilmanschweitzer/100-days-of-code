353765d20b151d4a651245c4d7f9412574c42a8c
Tilman Schweitzer <schweitzer.tilman@gmail.com>
2022-06-26 22:16:37 +0200

    advent-of-code/2019: Solve day one of 2019

--- advent-of-code/2020/src/main/java/de/tilmanschweitzer/adventofcode/aoc2019/Day01.java
+++ advent-of-code/2020/src/main/java/de/tilmanschweitzer/adventofcode/aoc2019/Day01.java
@@ -6,18 +6,19 @@ import java.io.InputStream;
 import java.util.List;
 
 
+import static java.lang.ClassLoader.getPlatformClassLoader;
 import static java.lang.ClassLoader.getSystemResourceAsStream;
 
 public class Day01 extends AdventOfCodeDay<Integer> {
 
     @Override
     public long getResultOfFirstPuzzle(final List<Integer> inputNumbers) {
-        return 0;
+        return inputNumbers.stream().map(Day01::getFuelForMass).reduce(Long::sum).orElse(0L);
     }
 
     @Override
     public long getResultOfSecondPuzzle(final List<Integer> inputNumbers) {
-        return 0;
+        return inputNumbers.stream().map(Day01::getTotalFuelForMass).reduce(Long::sum).orElse(0L);
     }
 
 
@@ -29,6 +30,20 @@ public class Day01 extends AdventOfCodeDay<Integer> {
     @Override
     protected InputStream getInputAsStream() {
         return getSystemResourceAsStream("2019/day01-input.txt");
+    }
+
+
+    public static long getFuelForMass(long mass) {
+        return mass / 3 - 2;
+    }
+
+    public static long getTotalFuelForMass(long mass) {
+        final long fuel = getFuelForMass(mass);
+
+        if (fuel <= 0) {
+            return 0;
+        }
+        return fuel + getTotalFuelForMass(fuel);
     }
 
 }
