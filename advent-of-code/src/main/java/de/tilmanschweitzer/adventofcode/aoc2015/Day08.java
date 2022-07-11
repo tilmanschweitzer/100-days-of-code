c154b3a430e340a242768e690c4d2e3019348f31
Tilman Schweitzer <schweitzer.tilman@gmail.com>
2022-07-11 15:28:47 +0200

    advent-of-code/2015: Solution for day eight part one

--- advent-of-code/src/main/java/de/tilmanschweitzer/adventofcode/aoc2015/Day08.java
+++ advent-of-code/src/main/java/de/tilmanschweitzer/adventofcode/aoc2015/Day08.java
@@ -5,7 +5,6 @@ import de.tilmanschweitzer.adventofcode.day.MultiLineAdventOfCodeDay;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.ClassLoader.getSystemResourceAsStream;
@@ -14,8 +13,8 @@ public class Day08 extends MultiLineAdventOfCodeDay<String> {
 
     @Override
     public long getResultOfFirstPuzzle(final List<String> input) {
-        final Integer sumOfInputChars = input.stream().map(Day08::numberOfInputChars).reduce(Integer::sum).orElse(0);
-        final Integer sumOfParsedChars = input.stream().map(Day08::numberOfParsedChars).reduce(Integer::sum).orElse(0);
+        final Integer sumOfInputChars = input.stream().map(Day08::countInputChars).reduce(Integer::sum).orElse(0);
+        final Integer sumOfParsedChars = input.stream().map(Day08::countParsedChars).reduce(Integer::sum).orElse(0);
         return sumOfInputChars - sumOfParsedChars;
     }
 
@@ -24,25 +23,72 @@ public class Day08 extends MultiLineAdventOfCodeDay<String> {
         return 0;
     }
 
-    public static int numberOfInputChars(String s) {
+    public static int countInputChars(String s) {
         return s.length();
     }
 
-    public static int numberOfParsedChars(final String s) {
-        return parseString(s).length();
+    public static int countParsedChars(final String input) {
+        return parseString(input).length();
     }
 
-    public static String parseString(String s) {
-        final Pattern unicodeHexChar = Pattern.compile("\\\\x([\\da-fA-F]{2})");
-        final Matcher unicodeHexCharMatcher = unicodeHexChar.matcher(s);
-        while (unicodeHexCharMatcher.find()) {
-            final String match = unicodeHexCharMatcher.group(0);
-            final String hexValue = unicodeHexCharMatcher.group(1);
-            final String replacement = new String(new byte[]{(byte) Integer.parseInt(hexValue, 16)}, StandardCharsets.UTF_8);
-            s = s.replace(match, replacement);
+    public static String parseString(String input) {
+        String remainingInput = input;
+
+        int remainingCharIndex = 0;
+        int charIndex = 0;
+
+        while (charIndex < input.length()) {
+            if (charIndex == 0) {
+                final char firstChar = remainingInput.charAt(remainingCharIndex);
+                if (firstChar == '\"') {
+                    remainingInput = remainingInput.substring( 1);
+                } else {
+                    throw new RuntimeException("Unexpected first char: " + firstChar);
+                }
+                charIndex++;
+                continue;
+            }
+            if (remainingCharIndex == remainingInput.length() - 1) {
+                final char lastChar = remainingInput.charAt(remainingCharIndex);
+                if (lastChar == '\"') {
+                    remainingInput = remainingInput.substring(0,  remainingInput.length() - 1);
+                } else {
+                    throw new RuntimeException("Unexpected last char: " + lastChar);
+                }
+                charIndex++;
+                continue;
+            }
+            if (remainingCharIndex < remainingInput.length() - 2 && remainingInput.charAt(remainingCharIndex) == '\\') {
+                final char nextChar = remainingInput.charAt(remainingCharIndex + 1);
+                final String previousChars = remainingInput.substring(0, remainingCharIndex);
+                if (nextChar == '\\') {
+                    remainingInput = previousChars + remainingInput.substring(remainingCharIndex + 1);
+                    charIndex++;
+                } else if (nextChar == '\"') {
+                    remainingInput = previousChars + remainingInput.substring(remainingCharIndex + 1);
+                    charIndex++;
+                } else if (nextChar == 'x') {
+                    final String unicodeSequence = remainingInput.substring(remainingCharIndex, remainingCharIndex + 4);
+                    System.out.println(unicodeSequence);
+                    if (unicodeSequence.matches("\\\\x([\\da-fA-F]{2})")) {
+                        remainingInput = previousChars + unicodeChar(unicodeSequence) + remainingInput.substring(remainingCharIndex + 4);
+                    }
+                    charIndex += 2;
+                }
+            }
+            remainingCharIndex++;
+            charIndex++;
         }
-        final String unescapedString = s.replaceAll("\\\\\"", "\"");
-        return unescapedString.substring(1, unescapedString.length() - 1);
+
+
+        return remainingInput;
+    }
+
+    private static String unicodeChar(String unicodeHex) {
+        if (!unicodeHex.matches("\\\\x([\\da-fA-F]{2})")) {
+            throw new RuntimeException("Unexpected unicode hex sequence: " + unicodeHex);
+        }
+        return new String(new byte[]{(byte) Integer.parseInt(unicodeHex.substring(2), 16)}, StandardCharsets.UTF_8);
     }
 
     @Override
--- advent-of-code/src/test/java/de/tilmanschweitzer/adventofcode/aoc2015/Day08Test.java
+++ advent-of-code/src/test/java/de/tilmanschweitzer/adventofcode/aoc2015/Day08Test.java
@@ -7,20 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class Day08Test {
 
     @Test
-    public void numberOfInputChars() {
-        assertEquals(2, Day08.numberOfInputChars("\"\""));
-        assertEquals(5, Day08.numberOfInputChars("\"abc\""));
-        assertEquals(10, Day08.numberOfInputChars("\"aaa\\\"aaa\""));
-        assertEquals(6, Day08.numberOfInputChars("\"\\x27\""));
+    public void countInputChars() {
+        assertEquals(2, Day08.countInputChars("\"\""));
+        assertEquals(5, Day08.countInputChars("\"abc\""));
+        assertEquals(10, Day08.countInputChars("\"aaa\\\"aaa\""));
+        assertEquals(6, Day08.countInputChars("\"\\x27\""));
+        assertEquals(6, Day08.countInputChars("\"x\\\\x\""));
+        assertEquals(4, Day08.countInputChars("\"\\\\\""));
+
     }
 
     @Test
-    public void numberOfInterpretedChars() {
-        assertEquals(0, Day08.numberOfParsedChars("\"\""));
-        assertEquals(3, Day08.numberOfParsedChars("\"abc\""));
-        assertEquals(7, Day08.numberOfParsedChars("\"aaa\\\"aaa\""));
-        assertEquals(1, Day08.numberOfParsedChars("\"\\x27\""));
-        assertEquals(1, Day08.numberOfParsedChars("\"\\\\\""));
+    public void countParsedChars() {
+        assertEquals(0, Day08.countParsedChars("\"\""));
+        assertEquals(3, Day08.countParsedChars("\"abc\""));
+        assertEquals(7, Day08.countParsedChars("\"aaa\\\"aaa\""));
+        assertEquals(1, Day08.countParsedChars("\"\\x27\""));
+        assertEquals(3, Day08.countParsedChars("\"x\\\\x\""));
+        assertEquals(1, Day08.countParsedChars("\"\\\\\""));
+        assertEquals("d\\gkbqo\\fwukyxab\"u".length(), Day08.countParsedChars("\"d\\\\gkbqo\\\\fwukyxab\\\"u\""));
+        assertEquals("kbngyfvvsdismznhar\\p\"\"gpryt\"jaeh".length(), Day08.countParsedChars("\"kbngyfvvsdismznhar\\\\p\\\"\\\"gpryt\\\"jaeh\""));
+        assertEquals("jcrkptrsasjp\\\"cwigzynjgspxxv\\vyb".length(), Day08.countParsedChars("\"jcrkptrsasjp\\\\\\\"cwigzynjgspxxv\\\\vyb\""));
     }
 
     @Test
@@ -30,5 +37,6 @@ class Day08Test {
         assertEquals("abc", Day08.parseString("\"abc\""));
         assertEquals("aaa\"aaa", Day08.parseString("\"aaa\\\"aaa\""));
         assertEquals("'", Day08.parseString("\"\\x27\""));;
+        assertEquals("d\\gkbqo\\fwukyxab\"u", Day08.parseString("\"d\\\\gkbqo\\\\fwukyxab\\\"u\""));
     }
 }
