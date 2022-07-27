5ce1967a1ec113840c0cae2abcf907ad492e7532
Tilman Schweitzer <schweitzer.tilman@gmail.com>
2022-07-27 22:18:24 +0200

    advent-of-code/2015: Partial solution for day 19 part two

--- advent-of-code/src/main/java/de/tilmanschweitzer/adventofcode/aoc2015/Day19RudolphMedicine.java
+++ advent-of-code/src/main/java/de/tilmanschweitzer/adventofcode/aoc2015/Day19RudolphMedicine.java
@@ -1,19 +1,17 @@
 package de.tilmanschweitzer.adventofcode.aoc2015;
 
+import com.google.common.collect.Lists;
 import de.tilmanschweitzer.adventofcode.day.MultiLineAdventOfCodeDay;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.io.InputStream;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static de.tilmanschweitzer.adventofcode.common.CollectionFunctions.sum;
 import static java.lang.ClassLoader.getSystemResourceAsStream;
@@ -27,7 +25,6 @@ public class Day19RudolphMedicine extends MultiLineAdventOfCodeDay<String, Integ
     public static Set<String> distinctReplacements(final String molecule, List<Replacement> replacements) {
         final Map<String, List<Replacement>> replacementsByMatch = replacements.stream().collect(Collectors.groupingBy(Replacement::getMatch));
         final Set<String> results = new HashSet<>();
-
 
         final Set<String> matchers = replacementsByMatch.keySet();
 
@@ -50,6 +47,59 @@ public class Day19RudolphMedicine extends MultiLineAdventOfCodeDay<String, Integ
         return results;
     }
 
+    public static List<ReplacementAtPosition> findStepsToGenerate(String start, String molecule, List<Replacement> replacements) {
+        final Map<String, List<Replacement>> replacementsByReplacement = replacements.stream().collect(Collectors.groupingBy(Replacement::getReplacement));
+
+        final List<List<ReplacementAtPosition>> possibleSteps = findReplacements(start, molecule, Collections.emptyList(), replacementsByReplacement).collect(Collectors.toUnmodifiableList());
+
+        for (List<ReplacementAtPosition> possibleStep : possibleSteps) {
+            System.out.println(possibleStep);
+        }
+
+        return possibleSteps.stream().sorted(Comparator.comparingInt(List::size)).findFirst().orElse(Collections.emptyList());
+    }
+
+    public static Stream<List<ReplacementAtPosition>> findReplacements(String start, String currentString, List<ReplacementAtPosition> currentSteps, Map<String, List<Replacement>> replacementsByReplacement ) {
+        if (currentString.equals(start)) {
+            return Stream.of(Lists.reverse(currentSteps));
+        }
+
+        if (currentString.contains(start) && !currentString.equals(start)) {
+            return Stream.empty();
+        }
+
+        if (currentSteps.size() > 1000) {
+            System.out.println("Max steps reached: " + currentString + " " + currentSteps);
+            return Stream.empty();
+        }
+
+
+        final Set<String> replacements = replacementsByReplacement.keySet();
+
+        final Set<List<ReplacementAtPosition>> results = new HashSet<>();
+
+        for (final String replacementString : replacements) {
+            String restMolecule = currentString;
+            String parsedMolecule = "";
+            int matcherIndex = restMolecule.indexOf(replacementString);
+            while (matcherIndex >= 0) {
+                for (Replacement replacement : replacementsByReplacement.get(replacementString)) {
+                    final String result = parsedMolecule + restMolecule.replaceFirst(replacementString, replacement.getMatch());
+                    final int replacementPosition = matcherIndex + parsedMolecule.length();
+                    final List<ReplacementAtPosition> nextSteps = new ArrayList<>(currentSteps);
+                    nextSteps.add(new ReplacementAtPosition(replacementPosition, replacement));
+                    findReplacements(start, result, nextSteps, replacementsByReplacement).forEach(results::add);
+                }
+                final int parsedUntil = matcherIndex + replacementString.length();
+                parsedMolecule = parsedMolecule + restMolecule.substring(0, parsedUntil);
+                restMolecule = restMolecule.substring(parsedUntil);
+                matcherIndex = restMolecule.indexOf(replacementString);
+            }
+        }
+
+        return results.stream();
+    }
+
     @Override
     public Integer getResultOfFirstPuzzle(final List<String> input) {
         final List<Replacement> replacements = input.subList(0, input.size() - 2).stream().map(Replacement::parse).collect(Collectors.toUnmodifiableList());
@@ -58,8 +108,10 @@ public class Day19RudolphMedicine extends MultiLineAdventOfCodeDay<String, Integ
     }
 
     @Override
-    public Integer getResultOfSecondPuzzle(final List<String> inputNumbers) {
-        return 0;
+    public Integer getResultOfSecondPuzzle(final List<String> input) {
+        final List<Replacement> replacements = input.subList(0, input.size() - 2).stream().map(Replacement::parse).collect(Collectors.toUnmodifiableList());
+        final String molecule = input.get(input.size() - 1);
+        return findStepsToGenerate("e", molecule, replacements).size();
     }
 
     @Override
@@ -73,9 +125,24 @@ public class Day19RudolphMedicine extends MultiLineAdventOfCodeDay<String, Integ
     }
 
 
+    @EqualsAndHashCode
+    public static class ReplacementAtPosition {
+        final int position;
+        final Replacement replacement;
+
+        public ReplacementAtPosition(int position, Replacement replacement) {
+            this.position = position;
+            this.replacement = replacement;
+        }
+
+
+        @Override
+        public String toString() {
+            return "{"+ position + ", " + replacement + "}";
+        }
+    }
 
     @EqualsAndHashCode
-    @ToString
     @Getter
     public static class Replacement {
         final String match;
@@ -93,6 +160,11 @@ public class Day19RudolphMedicine extends MultiLineAdventOfCodeDay<String, Integ
                 throw new RuntimeException("Could not parse line: " + line);
             }
             return new Replacement(matcher.group(1), matcher.group(2));
+        }
+
+        @Override
+        public String toString() {
+            return match + " => " + replacement;
         }
     }
 }
--- advent-of-code/src/test/java/de/tilmanschweitzer/adventofcode/aoc2015/Day19RudolphMedicineTest.java
+++ advent-of-code/src/test/java/de/tilmanschweitzer/adventofcode/aoc2015/Day19RudolphMedicineTest.java
@@ -1,9 +1,11 @@
 package de.tilmanschweitzer.adventofcode.aoc2015;
 
 import de.tilmanschweitzer.adventofcode.aoc2015.Day19RudolphMedicine.Replacement;
+import de.tilmanschweitzer.adventofcode.aoc2015.Day19RudolphMedicine.ReplacementAtPosition;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,6 +13,16 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day19RudolphMedicineTest {
+
+    @Test
+    public void getResultOfFirstPuzzle() {
+        assertEquals(518, new Day19RudolphMedicine().getResultOfFirstPuzzle());
+    }
+
+    @Test
+    public void getResultOfSecondPuzzle() {
+        assertEquals(0, new Day19RudolphMedicine().getResultOfSecondPuzzle());
+    }
 
     @Test
     public void parse() {
@@ -28,5 +40,26 @@ class Day19RudolphMedicineTest {
         Set<String> result = Day19RudolphMedicine.distinctReplacements("HOH", replacements);
 
         assertEquals(Set.of("HOOH", "HOHO", "OHOH", "HHHH"), result);
+    }
+
+    @Test
+    public void findStepsToGenerate() {
+        final List<Replacement> replacements = Stream.of("e => H", "e => O", "H => HO", "H => OH", "O => HH")
+                .map(Replacement::parse)
+                .collect(Collectors.toUnmodifiableList());
+
+        final List<ReplacementAtPosition> result = Day19RudolphMedicine.findStepsToGenerate("e", "HOH", replacements);
+
+        final List<ReplacementAtPosition> expectedResult = List.of(
+            new ReplacementAtPosition(0, new Replacement("e", "O")),
+            new ReplacementAtPosition(0, new Replacement("O", "HH")),
+            new ReplacementAtPosition(0, new Replacement("H", "HO"))
+        );
+
+        assertEquals(expectedResult, result);
+
+        final List<ReplacementAtPosition> result2 = Day19RudolphMedicine.findStepsToGenerate("e", "HOHOHO", replacements);
+
+        assertEquals(6, result2.size());
     }
 }
