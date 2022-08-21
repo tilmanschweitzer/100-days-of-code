233f3bc6eda3fe6da0788063074a10e612dc668c
Tilman Schweitzer <schweitzer.tilman@gmail.com>
2022-08-21 23:29:52 +0200

    advent-of-code/2017: Solve day 5

---
+++ advent-of-code/src/test/java/de/tilmanschweitzer/adventofcode/aoc2017/Day05Test.java
@@ -0,0 +1,38 @@
+package de.tilmanschweitzer.adventofcode.aoc2017;
+
+import de.tilmanschweitzer.adventofcode.aoc2017.Day05.AbstractJumpInstruction;
+import de.tilmanschweitzer.adventofcode.aoc2017.Day05.EventStrangerJumpInstruction;
+import de.tilmanschweitzer.adventofcode.aoc2017.Day05.StrangeJumpInstruction;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day05Test {
+
+    @Test
+    public void getResultOfFirstPuzzle() {
+        assertEquals(378980, new Day05().getResultOfFirstPuzzle());
+    }
+
+    @Test
+    public void getResultOfSecondPuzzle() {
+        assertEquals(26889114, new Day05().getResultOfSecondPuzzle());
+    }
+
+    @Test
+    public void countStepsToExitWithStrangeJumpInstruction() {
+        final List<AbstractJumpInstruction> instructions = Stream.of(0, 3, 0, 1, -3).map(StrangeJumpInstruction::new).collect(Collectors.toList());
+        assertEquals(5, Day05.countStepsToExit(instructions));
+    }
+
+    @Test
+    public void countStepsToExitWithEventStrangerJumpInstruction() {
+        final List<AbstractJumpInstruction> instructions = Stream.of(0, 3, 0, 1, -3).map(EventStrangerJumpInstruction::new).collect(Collectors.toList());
+        assertEquals(10, Day05.countStepsToExit(instructions));
+    }
+
+}
