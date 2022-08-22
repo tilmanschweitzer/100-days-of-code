d13d0cf9ce9b9d6c5e3025e33797333a6d33ea24
Tilman Schweitzer <schweitzer.tilman@gmail.com>
2022-08-22 21:57:56 +0200

    advent-of-code/2017: Solve day 6 part 1

---
+++ advent-of-code/src/test/java/de/tilmanschweitzer/adventofcode/aoc2017/Day06Test.java
@@ -0,0 +1,25 @@
+package de.tilmanschweitzer.adventofcode.aoc2017;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day06Test {
+
+
+    @Test
+    public void findCombinationsUntilInfiniteLoopIsDetected() {
+        final Set<List<Integer>> expectedResult = Set.of(
+            List.of(0, 2, 7, 0),
+                List.of(2, 4, 1, 2),
+                List.of(3, 1, 2, 3),
+                List.of(0, 2, 3, 4),
+                List.of(1, 3, 4, 1)
+        );
+
+        assertEquals(expectedResult, Day06.MemoryBlocks.from(List.of(0, 2, 7, 0)).findCombinationsUntilInfiniteLoopIsDetected());
+    }
+}
